44d3495f3f54ef060c4a3e9ff30d263f4298214a
Saggitel <lazba.timur@gmail.com>
2022-08-15 23:04:51 +0300

    Спринт 5 Правки

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -39,7 +39,7 @@ class Post(models.Model):
     )
 
     class Meta:
-        ordering = ['-pub_date']
+        ordering = ('-pub_date',)
 
     def __str__(self):
         return self.text[:15]
--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -108,7 +108,21 @@ class PostPagesTests(TestCase):
                 self.assertEqual(
                     post_title, f'Профайл пользователя {self.user}'
                 )
-
+    def test_post_detail_uses_correct_context(self): 
+        """Шаблон post_detail сформирован с правильным контекстом.""" 
+        NUM_OF_TEXTS_SYMBOLS_IN_TITLE = 30 
+        response = self.authorized_client.get( 
+            reverse('posts:post_detail', kwargs={ 
+                'post_id': f'{self.post.id}' 
+            })) 
+        post = response.context['post'] 
+        if post.author == self.user: 
+            post_title = post.text[:NUM_OF_TEXTS_SYMBOLS_IN_TITLE] 
+            self.assertEqual( 
+                post_title, self.post.text[:NUM_OF_TEXTS_SYMBOLS_IN_TITLE] 
+            ) 
+            self.assertEqual(post, self.post) 
+                
     def test_post_create_uses_correct_context(self):
         """Шаблон post_create сформирован с правильным контекстом."""
         response = self.authorized_client.get(
